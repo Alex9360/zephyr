79b75c2e7b74bb279108ba0231f1890853639001
Sanjay Vallimanalan <sanjay@linumiz.com>
2026-04-01 14:54:42 +0530

    soc: infineon: cat1c: add CYT4DN SoC support
    Added support for Infineon's CAT1C based CYT4DN series SoC
    
    Signed-off-by: Sanjay Vallimanalan <sanjay@linumiz.com>
    Signed-off-by: Christoph Seitz <christoph.seitz@infineon.com>

---
+++ soc/infineon/cat1c/cyt4dn/soc.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2026 Linumiz
+ * Copyright (c) 2026 Infineon Technologies AG
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @brief Infineon CAT1C SOC.
+ */
+
+#ifndef _SOC__H_
+#define _SOC__H_
+
+#ifndef _ASMLANGUAGE
+#include <cy_device_headers.h>
+#include <cy_syslib.h>
+#endif /* !_ASMLANGUAGE */
+
+#endif /* _SOC__H_ */
