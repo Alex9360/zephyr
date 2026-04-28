6952854ae9d4467d68cc2576bd019aab3709060d
Alexpandi Muniyandi <alexpandi@linumiz.com>
2026-04-28 18:50:51 +0530

    wip: fixing pr comments
    Signed-off-by: Alexpandi Muniyandi <alexpandi@linumiz.com>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -5,10 +5,17 @@
  */
 
 #include <stdio.h>
+#include <stdint.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
 
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+	uint32_t reset_vec = *(volatile uint32_t *)
+                        (DT_REG_ADDR(DT_NODELABEL(flash_m7_1)) + 4);
+	printf("reset_vec = %x\n", reset_vec);
+
 
 	return 0;
 }
---
+++ snippets/infineon/t2g-c2d6m-cm7-core1/README.rst
@@ -0,0 +1,12 @@
+.. _infineon-cat1-m7_1:
+
+Infineon CAT1 M7_1 snippet to place application in M7_1 memory
+##############################################################
+
+Overview
+********
+
+This snippet allows users to build Zephyr with the capability to run the 
+application on the M7_1 core. It selects the correct memory regions for 
+the M7_1 core.
+
