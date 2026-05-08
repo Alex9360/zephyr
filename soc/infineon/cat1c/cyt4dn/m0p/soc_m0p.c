6ca2d401974c41cdf1bdce71f7e20603c5d67456
Sanjay Vallimanalan <sanjay@linumiz.com>
2026-05-08 13:20:19 +0530

    soc: infineon: cat1c: cyt4dn: add SROM API support
    Added SROM API support for CYT4DN. The SROM APIs can only be
    executed by the M0P core. The SROM APIs are essential for
    the flash controller to work as the flash write and erase
    commands can only work by executing SROM APIs.
    
    Signed-off-by: Sanjay Vallimanalan <sanjay@linumiz.com>

--- soc/infineon/cat1c/cyt4dn/m0p/soc_m0p.c
+++ soc/infineon/cat1c/cyt4dn/m0p/soc_m0p.c
@@ -16,17 +16,71 @@
 
 #include <cy_wdt.h>
 #include <cy_sysclk.h>
+#include <cy_sysint.h>
 
 #define M7_0_FREQ DT_PROP(DT_NODELABEL(m7_0), clock_frequency)
 #define M7_1_FREQ DT_PROP(DT_NODELABEL(m7_1), clock_frequency)
 
 #define IFX_FAST_CLOCK_DOMAIN_FREQ_MHZ (MAX(M7_0_FREQ, M7_1_FREQ) / 1000000)
 
+void cat1c_srom_syscall_isr(void *arg)
+{
+	/* Trigger IRQ0 in Software by writing to ISPR register */
+	NVIC_SetPendingIRQ(NvicMux0_IRQn);
+	/* Read back the register to ensure that the write has happened */
+	NVIC->ISPR[0U];
+	/* Clear the NVIC Pending bit of IRQ0. This is done as a fallback in case the system call
+	was suppressed (e.g., by disabled interrupts) */
+	NVIC_ClearPendingIRQ(NvicMux0_IRQn);
+	/* Read back the register to ensure that the write has happened */
+	NVIC->ICPR[0U];
+}
+
+static void cat1c_m0p_srom_init()
+{
+#if defined(CONFIG_SRAM_VECTOR_TABLE)
+	volatile uint32_t *const volatile vectorTable = (uint32_t *)_sram_vector_start;
+#else
+	volatile const uint32_t *const volatile vectorTable = (uint32_t *)_vector_start;
+#endif
+	/* The array syntax is necessary to avoid out-of-bounds warnings in some compilers. */
+	volatile uint32_t(*const volatile sromTable)[32] = (uint32_t(*)[32])0x00000000;
+
+#if defined(CONFIG_SRAM_VECTOR_TABLE)
+	/* Use IRQ0 and IRQ1 handlers from SROM vector table */
+	vectorTable[16] = (*sromTable)[16];
+	vectorTable[17] = (*sromTable)[17];
+#else
+	/* If the vector table can't be changed, check the entries. */
+	if (vectorTable[16] != (*sromTable)[16] || vectorTable[17] != (*sromTable)[17]) {
+		k_fatal_halt(K_ERR_CPU_EXCEPTION);
+	}
+#endif
+
+	/* Setup SROM API IRQs */
+	/* IRQ0 & IRQ1 are used as entry to the SROM APIs.
+	 * IRQ2 is used as trampoline into handler mode as
+	 * required by the SROM API. Priority are setup as
+	 * described in the TRM. */
+	NVIC_SetPriority(NvicMux0_IRQn, 0);
+	NVIC_SetPriority(NvicMux1_IRQn, 0);
+	NVIC_SetPriority(NvicMux2_IRQn, 1);
+	NVIC_EnableIRQ(NvicMux0_IRQn);
+	NVIC_EnableIRQ(NvicMux1_IRQn);
+	NVIC_EnableIRQ(NvicMux2_IRQn);
+
+	/* Move IPC SROM API trigger to IRQ2 for the trampoline function. */
+	Cy_SysInt_EnableSystemInt(cpuss_interrupts_ipc_0_IRQn);
+	Cy_SysInt_SetInterruptSource(NvicMux0_IRQn, cpuss_interrupts_ipc_0_IRQn);
+	Cy_SysInt_SetInterruptSource(NvicMux1_IRQn, cpuss_interrupts_ipc_1_IRQn);
+}
+
 void soc_prep_hook(void)
 {
 	Cy_WDT_Unlock();
 	Cy_WDT_Disable();
 	SystemCoreClockUpdate();
+	cat1c_m0p_srom_init();
 	Cy_SysLib_SetWaitStates(false, IFX_FAST_CLOCK_DOMAIN_FREQ_MHZ);
 }
 
