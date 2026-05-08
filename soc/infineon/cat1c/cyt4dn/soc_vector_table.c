09f736796d5096f4a111424b00afd4c89b92a7bd
Sanjay Vallimanalan <sanjay@linumiz.com>
2026-05-08 13:20:19 +0530

    soc: infineon: cat1c: cyt4dn: add SROM API support
    Added SROM API support for CYT4DN. The SROM APIs can only be
    executed by the M0P core. The SROM APIs are essential for
    the flash controller to work as the flash write and erase
    commands can only work by executing SROM APIs.
    
    Signed-off-by: Sanjay Vallimanalan <sanjay@linumiz.com>

--- soc/infineon/cat1c/cyt4dn/soc_vector_table.c
+++ soc/infineon/cat1c/cyt4dn/soc_vector_table.c
@@ -9,10 +9,20 @@
 
 #define CPU_IRQS 16	/* M0P and M7 has 8 NVIC Lines and 8 Internal IRQs */
 
+#if defined(CONFIG_SOC_CYT4DN_CORE_M0PLUS)
+extern void cat1c_srom_syscall_isr(void);
+#endif
+
 const uintptr_t __irq_vector_table _irq_vector_table[CPU_IRQS] = {
+#if defined(CONFIG_SOC_CYT4DN_CORE_M0PLUS)
+	((uintptr_t)0x49),
+	((uintptr_t)0x281),
+	((uintptr_t)cat1c_srom_syscall_isr),
+#else
 	((uintptr_t)_isr_wrapper),
 	((uintptr_t)_isr_wrapper),
 	((uintptr_t)_isr_wrapper),
+#endif
 	((uintptr_t)_isr_wrapper),
 	((uintptr_t)_isr_wrapper),
 	((uintptr_t)_isr_wrapper),
