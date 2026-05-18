7ac645eca092612cd811fd2b0f61c87a7cd5782e
Alexpandi Muniyandi <alexpandi@linumiz.com>
2026-05-18 15:25:26 +0530

    boards: infineon: add cytvii_b_h_8m_320_cpu
    Add board support for the Infineon T2G CYTVII-B-H-8M-320-CPU
    evaluation board, based on the CYT4BFCCHE SoC.
    
    Signed-off-by: Alexpandi Muniyandi <alexpandi@linumiz.com>
    Signed-off-by: Varun AV <Varun.AV@infineon.com>

---
+++ boards/infineon/cytvii-b-h-8m-320-cpu/board.cmake
@@ -0,0 +1,12 @@
+# Copyright (c) 2026 Infineon Technologies AG,
+# or an affiliate of Infineon Technologies AG.
+# Copyright (c) 2026 Linumiz
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Connect to CM0P core
+board_runner_args(openocd "--target-handle=cat1c.cpu.cm0")
+board_runner_args(jlink "--device=CYT4BFCCHE" "--speed=4000")
+
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
