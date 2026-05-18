c5da42e9e26bc759bc87924233872ad597e4fbe6
Sanjay Vallimanalan <sanjay@linumiz.com>
2026-05-18 15:35:15 +0530

    soc: infineon: cat1a: add CYT2BL and CYT2B7 SoC support
    Added support for Infineon's CAT1A based CYT2BL and CYT2B7
    series SoCs.
    
    Signed-off-by: Sanjay Vallimanalan <sanjay@linumiz.com>
    Signed-off-by: Christoph Seitz <christoph.seitz@infineon.com>
    Signed-off-by: Shreyas G Khanapur <ShreyasG.Khanapur@infineon.com>
    Signed-off-by: Pranati-IFX <Pranati.hota@infineon.com>

---
+++ soc/infineon/cat1a/cyt2bx/m4/soc_m4.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2026 Linumiz
+ * Copyright (c) 2026 Infineon Technologies AG
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @brief Infineon CAT1A SOC.
+ */
+
+#include <zephyr/init.h>
+#include <cy_wdt.h>
+
+void soc_prep_hook(void)
+{
+	Cy_WDT_Unlock();
+	Cy_WDT_Disable();
+}
+
+static int early_init() {
+	Cy_PDL_Init(CY_DEVICE_CFG);
+	SystemCoreClockUpdate();
+
+	return 0;
+}
+SYS_INIT(early_init, PRE_KERNEL_1, 0);
