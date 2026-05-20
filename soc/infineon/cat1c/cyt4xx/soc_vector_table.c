5da17ed583640381eacb671bacc816c0a36b36e5
Alexpandi Muniyandi <alexpandi@linumiz.com>
2026-05-20 16:53:45 +0530

    soc: infineon: cat1c: rename cyt4dn to cyt4xx
    Rename the cyt4dn SoC directory and Kconfig symbols to cyt4xx so
    the series can host multiple TRAVEO T2G CYT4 parts sharing the
    same startup and PDL code.
    
    Kconfig symbol renames:
      SOC_SERIES_CYT4DN       -> SOC_SERIES_CYT4XX
      SOC_CYT4DN_M7           -> SOC_CYT4XX_M7
      SOC_CYT4DN_CORE_M0PLUS  -> SOC_CYT4XX_CORE_M0PLUS
      SOC_CYT4DN_CORE_M7_0    -> SOC_CYT4XX_CORE_M7_0
      SOC_CYT4DN_CORE_M7_1    -> SOC_CYT4XX_CORE_M7_1
      SOC_CYT4DN_START_M7_0   -> SOC_CYT4XX_START_M7_0
      SOC_CYT4DN_START_M7_1   -> SOC_CYT4XX_START_M7_1
    
    Signed-off-by: Alexpandi Muniyandi <alexpandi@linumiz.com>

--- soc/infineon/cat1c/cyt4xx/soc_vector_table.c
+++ soc/infineon/cat1c/cyt4xx/soc_vector_table.c
@@ -9,12 +9,12 @@
 
 #define CPU_IRQS 16	/* M0P and M7 has 8 NVIC Lines and 8 Internal IRQs */
 
-#if defined(CONFIG_SOC_CYT4DN_CORE_M0PLUS)
+#if defined(CONFIG_SOC_CYT4XX_CORE_M0PLUS)
 extern void cat1c_srom_syscall_isr(void);
 #endif
 
 const uintptr_t __irq_vector_table _irq_vector_table[CPU_IRQS] = {
-#if defined(CONFIG_SOC_CYT4DN_CORE_M0PLUS)
+#if defined(CONFIG_SOC_CYT4XX_CORE_M0PLUS)
 	((uintptr_t)0x49),
 	((uintptr_t)0x281),
 	((uintptr_t)cat1c_srom_syscall_isr),
